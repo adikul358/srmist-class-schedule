184fb9472bc881c90df740245595126fd61c536a
Aditya Kulshrestha <adi.kul358@gmail.com>
2024-01-06 20:47:22 +0530

    Change to SRMIST System

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from ics import Calendar, Event
 from ics.alarm import DisplayAlarm
 from datetime import datetime, timedelta
 from os import system
+import csv
 
 
 def next_monday():
     return datetime.now() + timedelta((0 - datetime.now().weekday()) % 7)
 
-
 slots = []
-classes = []
+classes = [[],[],[],[],[]]
 days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 
-with open('/users/aditya/dev/class-schedule/slots.csv', 'r+') as f:
+with open('./slots.csv', 'r+') as f:
     slots_raw = f.readlines()
     for i in slots_raw[1:]:
         i = i.strip("\n").split(",")
@@ -22,20 +22,19 @@ with open('/users/aditya/dev/class-schedule/slots.csv', 'r+') as f:
         }
         slots.append(slots_temp)
 
-with open('/users/aditya/dev/class-schedule/classes.csv', 'r+') as f:
-    classes_raw = f.readlines()
-    for i in classes_raw[1:]:
-        if i == "\n":
+with open('./classes.csv', 'r+') as f:
+    classes_raw = csv.reader(f, delimiter=',', quotechar='"')
+    for i in list(classes_raw)[1:]:
+        if i == []:
             continue
-        i = i.strip("\n").split(",")
         classes_temp = {
             'name': i[0],
-            'desc': i[3],
-            'day': i[2],
+            'desc': i[1],
         }
+        classes_temp["begin"] = slots[int(i[3])-1]["begin"]
+        classes_temp["end"] = slots[int(i[4])-1]["end"]
 
-        classes_temp.update(slots[int(i[1])])
-        classes.append(classes_temp)
+        classes[int(i[2])-1].append(classes_temp)
 
 
 nm_prompt = next_monday().strftime("%Y-%m-%d")
@@ -44,36 +43,39 @@ if init_date == "":
     init_date = nm_prompt
 elif datetime.strptime(init_date, "%Y-%m-%d").strftime("%w") != "1":
     raise ValueError("Entered date not a Monday")
-
+curr_date = datetime.strptime(init_date, "%Y-%m-%d")
+day_orders = [int(x) for x in input("Day Orders for the Week: ").split(" ")]
+print()
 c = Calendar()
 
-for i in classes:
-    e = Event()
-    date_begin_str = f'{init_date} {i["begin"]}+0530'
-    date_end_str = f'{init_date} {i["end"]}+0530'
+for i in day_orders:
+    if i != 0: 
+        for curr_class in classes[i-1]:
+            e = Event()
 
-    date_begin = datetime.strptime(
-        date_begin_str, "%Y-%m-%d %H%M%z") + timedelta(days.index(i['day'].lower()))
-    date_end = datetime.strptime(
-        date_end_str, "%Y-%m-%d %H%M%z") + timedelta(days.index(i['day'].lower()))
+            date_str = curr_date.strftime("%Y-%m-%d")
+            date_begin_str = f'{date_str} {curr_class["begin"]}+0530'
+            date_end_str = f'{date_str} {curr_class["end"]}+0530'
+            date_begin = datetime.strptime(date_begin_str, "%Y-%m-%d %H%M%z")
+            date_end = datetime.strptime(date_end_str, "%Y-%m-%d %H%M%z")
 
-    e.name = i['name']
-    e.begin = date_begin.strftime("%Y-%m-%d %H:%M:%S%z")
-    e.end = date_end.strftime("%Y-%m-%d %H:%M:%S%z")
-    e.description = i['desc']
-    e.alarms = [DisplayAlarm(trigger=timedelta(minutes=-5)),
-                DisplayAlarm(trigger=date_begin)]
+            e.name = curr_class['name']
+            e.begin = date_begin.strftime("%Y-%m-%d %H:%M:%S%z")
+            e.end = date_end.strftime("%Y-%m-%d %H:%M:%S%z")
+            e.location = curr_class['desc']
+            e.alarms = [DisplayAlarm(trigger=date_begin)]
+            # DisplayAlarm(trigger=timedelta(minutes=-10)),
+                        
+            c.events.add(e)
 
-    if i['name'] != "Economics B":
-        e.attendees = ['dubey.ona@gmail.com']
-
-    c.events.add(e)
-
-    print(f'Created {i["name"]} {datetime.strftime(date_begin, "%Y-%m-%d %H:%M:%S")} - {datetime.strftime(date_end, "%Y-%m-%d %H:%M:%S")}')
+            print(f'Created  {date_str} {curr_class["begin"]}-{curr_class["end"]} {curr_class["name"]}')
+    
+    curr_date += timedelta(1)
 
 with open('out.ics', 'w') as my_file:
     my_file.writelines(c)
 
+print()
 print()
 print("Exported out.ics")
 
